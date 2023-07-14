e43d131d0e5c2c95049b5b9a99dde11bdc0d4477
Chisel <chisel@malik-wright.uk>
2023-07-14 13:50:16 +0100

    refactor helper/last-change.py
    it got a bit messy, so I made functions

--- helper/last-change.py
+++ helper/last-change.py
@@ -29,37 +29,44 @@ def parse_changelog(changelog):
     return {"version": version, "changes": changes}
 
 
+def build_markdown_block(parsed_info: dict) -> str:
+    # build a string with the changes
+    markdown_block = ""
+    if parsed_info["changes"]["patch_changes"]:
+        markdown_block += "### Patch Changes\n"
+        for change in parsed_info["changes"]["patch_changes"]:
+            markdown_block += f"- {change}\n"
+    if parsed_info["changes"]["minor_changes"]:
+        markdown_block += "\nMinor Changes:\n"
+        for change in parsed_info["changes"]["minor_changes"]:
+            markdown_block += f"- {change}\n"
+    if parsed_info["changes"]["major_changes"]:
+        markdown_block += "\nMajor Changes:\n"
+        for change in parsed_info["changes"]["major_changes"]:
+            markdown_block += f"- {change}\n"
+
+    return markdown_block
+
+
+def output_markdown_block(markdown_block: str) -> None:
+    # if we have an env variable named GITHUB_OUTPUT, we are running in github
+    # actions and should use that as a file to append to, othwerwise just print to
+    # stdout
+    if "GITHUB_OUTPUT" in os.environ:
+        with open(os.environ["GITHUB_OUTPUT"], "a") as f:
+            # we know it's likely to be multiline, so we need to use a little magic
+            # that we found in https://github.com/github/docs/issues/21529
+            f.write("changeentry<<EOF\n")
+            f.write(f"{markdown_block}\n")
+            f.write("EOF\n")
+    else:
+        sys.stdout.write(f"[set-output] changeentry:\n{markdown_block}")
+
+
 # read CHANGELOG.md and save in changelog_text
 with open("CHANGELOG.md", "r", encoding="utf-8") as f:
     changelog_text = f.read()
 
 parsed_info = parse_changelog(changelog_text)
-
-# build a string with the changes
-markdown_block = ""
-if parsed_info["changes"]["patch_changes"]:
-    markdown_block += "### Patch Changes\n"
-    for change in parsed_info["changes"]["patch_changes"]:
-        markdown_block += f"- {change}\n"
-if parsed_info["changes"]["minor_changes"]:
-    markdown_block += "\nMinor Changes:\n"
-    for change in parsed_info["changes"]["minor_changes"]:
-        markdown_block += f"- {change}\n"
-if parsed_info["changes"]["major_changes"]:
-    markdown_block += "\nMajor Changes:\n"
-    for change in parsed_info["changes"]["major_changes"]:
-        markdown_block += f"- {change}\n"
-
-
-# if we have an env variable named GITHUB_OUTPUT, we are running in github
-# actions and should use that as a file to append to, othwerwise just print to
-# stdout
-if "GITHUB_OUTPUT" in os.environ:
-    with open(os.environ["GITHUB_OUTPUT"], "a") as f:
-        # we know it's likely to be multiline, so we need to use a little magic
-        # that we found in https://github.com/github/docs/issues/21529
-        f.write("changeentry<<EOF\n")
-        f.write(f"{markdown_block}\n")
-        f.write("EOF\n")
-else:
-    sys.stdout.write(f"[set-output] changeentry:\n{markdown_block}")
+markdown_block = build_markdown_block(parsed_info)
+output_markdown_block(markdown_block)
